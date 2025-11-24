9fec58c201fe3dfbba5a6b43017dab12827e0b3b
Ibuetab <ibuetab@gmail.com>
2025-11-24 20:29:17 +0100

    Added time_zone.py, basic_functions.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-import datetime
-import pytz
 from dotenv import load_dotenv
 from telegram import Update, Bot
 from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes
@@ -8,7 +6,7 @@ from telegram.ext import Application, ApplicationBuilder, CommandHandler, Contex
 #---------------------------------------------------------------------------------------------------
 
 from persistence import TASKLIST, REGISTERED_USERS, load_data
-
+from basic_functions import start, help
 
 #---------------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------
@@ -19,60 +17,6 @@ BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 if not BOT_TOKEN:
     raise ValueError("El token de Telegram no se encontró. Asegúrate de que BOT_TOKEN esté definido en el archivo .env.")
 
-#TIME ZONE
-#---------------------------------------------------------------------------------------------------
-#---------------------------------------------------------------------------------------------------
-
-ZONE = pytz.timezone("Europe/Madrid")
-time = datetime.time(hour=12, minute= 24, second=0, tzinfo=ZONE)
-
-
-
-#BASIC FUNCTIONS
-#---------------------------------------------------------------------------------------------------
-#---------------------------------------------------------------------------------------------------
-
-""" Due to Privacy Policy of Telegram, users must initialize the communication with the bot. 
-So, I tried to make a function which bot initialize the comunication with the user, but it is not possible.  
-
-"""
-
-async def start(update:Update, context):
-    global REGISTERED_USERS
-    global TASKLIST
-
-    chat_id = update.effective_chat.id #get the chat_id
-    user_id = update.effective_user.id #get the user id
-    username = update.effective_user.username #get the username
-
-    if chat_id not in REGISTERED_USERS:
-
-        REGISTERED_USERS[chat_id] = {
-            'user_id': user_id,
-            'username': username,
-            'fecha_registro': str(datetime.datetime.now(ZONE))
-        }
-
-        TASKLIST[chat_id] = []
-   
-        if username:
-            await context.bot.send_message(chat_id = chat_id, text=f"Hola, usuario @{username}, utiliza el comando /help para ver una lista de comandos disponibles")
-            print(f"DEBUG: Nuevo usuario registrado: @{username}")
-        else:
-            await context.bot.send_message(chat_id = chat_id, text=f"Hola, usuario {chat_id}, utiliza el comando /help para ver una lista de comandos disponibles")
-            print(f"DEBUG: Nuevo usuario registrado: @{username}")
-
-
-    else:
-        await context.bot.send_message(
-            chat_id=chat_id,
-            text="Ya estás registrado. Usa el comando /help para ver las opciones disponibles"
-        )
-
-
-async def help(update:Update, context):
-    chat_id= update.effective_chat.id
-    await context.bot.send_message(chat_id=chat_id, text=f"Aqui tienes una lista de comandos disponibles: \n /start -> Inicializa el bot \n /help -> Muestra una lista de comandos del bot \n /addtask (nombre de tarea)-> añade una tarea a la lista de tareas")
 
 
 #TASK FUNCTIONS
