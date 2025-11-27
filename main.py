1a584e1dffb54c3f4e7da5df307630783112d0d2
Ibuetab <ibuetab@gmail.com>
2025-11-27 20:41:39 +0100

    Added delete multiple tasks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, ConversationHandler, CallbackQueryHandler
 
 
 #LOCAL IMPORTS
@@ -8,7 +8,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler
 #---------------------------------------------------------------------------------------------------
 import persistence
 from basic_functions import start, help
-from task_functions import add_task, delete_task, show_pending_tasks
+from task_functions import add_task, show_pending_tasks, delete_task, delete_button, OPTION, cancel
 
 
 #BOT TOKEN
@@ -19,7 +19,6 @@ BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 if not BOT_TOKEN:
     raise ValueError("El token de Telegram no se encontró. Asegúrate de que BOT_TOKEN esté definido en el archivo .env.")
-
 
 
 #MAIN FUNCTION
@@ -39,8 +38,18 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help))
     app.add_handler(CommandHandler("addtask", add_task))
-    app.add_handler(CommandHandler("deltask", delete_task))
     app.add_handler(CommandHandler("showtasks", show_pending_tasks))
+
+    #app.add_handler(CommandHandler("deltask", delete_task))
+    del_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("deltask", delete_task)],
+        states = {
+            OPTION: [CallbackQueryHandler(delete_button)]
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+        per_message=False)
+    
+    app.add_handler(del_conv_handler)
    
    #---------------------------------------------------------------------------------------------------
     app.run_polling()
--- task_functions.py
+++ task_functions.py
@@ -1,4 +1,5 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CallbackContext, ConversationHandler
 
 
 #LOCAL IMPORTS
@@ -35,27 +36,6 @@ async def add_task(update:Update, context):
        await context.bot.send_message(chat_id=chat_id, text="Usa el comando /start primero")
 
 
-    
-async def delete_task(update:Update, context):
-
-    chat_id = update.effective_chat.id
-
-    task = " ".join(context.args)
-
-    if chat_id in persistence.TASKLIST:
-        user_tasklist = persistence.TASKLIST[chat_id]
-        if task not in user_tasklist:
-            await context.bot.send_message(chat_id=chat_id, text=f"{task} no existe en la lista de tareas")
-        
-        else:
-            persistence.TASKLIST[chat_id].remove(task)
-            await context.bot.send_message(chat_id=chat_id, text=f"La tarea {task} ha sido eliminada")
-
-    else:
-    
-        await context.bot.send_message(chat_id=chat_id, text=f"Usa el comando /start primero")
-
-
 async def show_pending_tasks(update:Update, context):
 
     chat_id = update.effective_chat.id
@@ -68,3 +48,76 @@ async def show_pending_tasks(update:Update, context):
     
     else:
         await context.bot.send_message(chat_id=chat_id, text=f"Usa el comando /start primero")
+
+
+
+
+OPTION = range(1)
+
+async def delete_task(update:Update, context:CallbackContext):
+
+    chat_id = update.effective_chat.id
+
+    if chat_id not in persistence.TASKLIST or not persistence.TASKLIST[chat_id].get("pending_tasks"):
+        await update.message.reply_text("¡No tienes tareas pendientes para borrar!")
+        return ConversationHandler.END 
+
+    else:
+        user_tasklist = persistence.TASKLIST[chat_id]["pending_tasks"]
+        keyboard = []
+        
+        #Create a row for each task
+        for index,task in enumerate(user_tasklist):
+                keyboard.append([InlineKeyboardButton(f"{task}", callback_data=task)])
+
+        
+        keyboard.append([InlineKeyboardButton("Cancelar", callback_data="CANCEL_DELETE")])
+        reply_markup = InlineKeyboardMarkup(keyboard)
+                
+        await update.message.reply_text(text=f"Tienes las siguientes tareas pendientes, ¿Cual quieres borrar?:", reply_markup=reply_markup)
+        return OPTION
+
+    
+
+async def delete_button(update:Update, context:CallbackContext):
+    chat_id = update.effective_chat.id
+    query = update.callback_query
+    await query.answer()
+
+    data = query.data
+
+    user_tasklist = persistence.TASKLIST[chat_id]["pending_tasks"]
+    
+
+    if chat_id in persistence.TASKLIST:
+
+        if data == "CANCEL_DELETE":
+            await query.edit_message_text("Operación de eliminación de tareas cancelada.")
+            return ConversationHandler.END
+
+        if data in user_tasklist:
+            user_tasklist.remove(data)
+
+        if not user_tasklist:
+        # No quedan tareas: Finalizar la conversación
+            await query.edit_message_text("✅ ¡Todas las tareas han sido eliminadas! Conversación finalizada.")
+            return ConversationHandler.END
+
+        keyboard = []
+        for task in user_tasklist:
+            keyboard.append([InlineKeyboardButton(f"{task}", callback_data=task)])
+        
+    keyboard.append([InlineKeyboardButton("Terminar Eliminación", callback_data="CANCEL_DELETE")])
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await query.edit_message_text(
+        "Tarea eliminada. Toca otra para seguir borrando:",
+        reply_markup=reply_markup
+    )
+
+    return OPTION
+
+
+async def cancel(update: Update, context: CallbackContext) -> int:
+    await update.message.reply_text("Operacion cancelada.")
+    return ConversationHandler.END
