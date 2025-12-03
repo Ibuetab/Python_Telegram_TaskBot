4eeb988af369e9f2ee45744168de91a76a131974
Ibuetab <ibuetab@gmail.com>
2025-12-03 21:20:22 +0100

    added get_day_frequency function

--- functions/reminders_functions.py
+++ functions/reminders_functions.py
@@ -35,3 +35,24 @@ async def get_reminder_name(update:Update, context: CallbackContext):
         
     return DAY
 
+
+#Solo muestra los botones
+async def get_day_frequency(update:Update, context:CallbackContext):
+    chat_id = update.effective_chat.id
+
+    keyboard = [
+        [InlineKeyboardButton(f"Lunes", callback_data="LU")],
+        [InlineKeyboardButton(f"Martes", callback_data="MA")],
+        [InlineKeyboardButton(f"Miércoles", callback_data="MI")],
+        [InlineKeyboardButton(f"Jueves", callback_data="JU")],
+        [InlineKeyboardButton(f"Viernes", callback_data="VI")],
+        [InlineKeyboardButton(f"Sábado", callback_data="SA")],
+        [InlineKeyboardButton(f"Domingo", callback_data="DO")],
+        [InlineKeyboardButton(f"Lunes a Viernes", callback_data="DAILY")],
+        [InlineKeyboardButton(f"Hecho", callback_data="DONE")],
+        [InlineKeyboardButton(f"Cancelar", callback_data="CANCEL")],
+    ]
+
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    return DAY
